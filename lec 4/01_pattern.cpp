59babc8ac716b966e0ab62d4aba117434b980fe2
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-02-10 21:52:38 +0530

    updated files

--- lec 4/01_pattern.cpp
+++ lec 4/01_pattern.cpp
@@ -5,8 +5,9 @@ int main() {
     int n;
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=1; j<=n-i+1;j++){
+        for(int j=1;j<=n;j++){
             cout<<"*";
+
         }
         cout<<endl;
     }
---
+++ lec 4/03_patterhollow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(i==1||i==n||j==1||j==n){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}

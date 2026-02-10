6a0d6135a0b36b68412cb96f816ce54dfc013916
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-02-10 23:00:00 +0530

    notes

--- lec 4/05_columnasymmetric.cpp
+++ lec 4/05_columnasymmetric.cpp
@@ -5,22 +5,21 @@ int main() {
     int n;
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
+        for(int j=1;j<=i-1;j++){
             cout<<"  ";
         }
-        for(int j=1;j<=i;j++){
+        for(int j=1;j<=n-i+1;j++){
             cout<<"* ";
         }
-       
-
-        for(int j=i-1;j>=1;j--){
+        for(int j=n-i;j>=1;j--){
             cout<<"* ";
         }
-        for(int j=1;j<=n-i;j++){
-            cout<<" ";
+        for(int j=1;j<=i-1;j++){
+            cout<<"  ";
         }
+         
         cout<<endl;
     }
-
     return 0;
-    } 
+
+}

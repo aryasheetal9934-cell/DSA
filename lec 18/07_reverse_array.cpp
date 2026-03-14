b3a504dc8625a1e9372080c1d0f14abec2ec7ffa
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-03-14 22:16:40 +0530

    notes

--- lec 18/07_reverse_array.cpp
+++ lec 18/07_reverse_array.cpp
@@ -1,22 +1,54 @@
+// #include <iostream>
+// using namespace std;
+// void printArr(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<",";
+//     }
+//     cout<<endl;
+// }
+
+// int main() {
+//     int arr[]{5,4,3,9,2};
+//     int n=sizeof(arr)/sizeof(int);
+//     int start=0,end=n-1;
+//     while(start<end){
+//         swap(arr[start],arr[end]);
+//         start++;
+//         end--;
+//     }
+//     printArr(arr,n);
+    
+//     return 0;
+// }
+
 #include <iostream>
+#include<algorithm>
 using namespace std;
-void printArr(int arr[],int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<",";
+
+void printarr(int arr[],int n){
+        reverse(arr,arr+n);
+        for(int i=0;i<n;i++){
+        cout<<arr[i];
+         }
+         
+         
     }
-    cout<<endl;
-}
 
 int main() {
-    int arr[]{5,4,3,9,2};
-    int n=sizeof(arr)/sizeof(int);
-    int start=0,end=n-1;
-    while(start<end){
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+    cin>>arr[i];
     }
-    printArr(arr,n);
+    // int start=0,end=n-1;
+    // while(start<end){
+    //     swap(arr[start],arr[end]);
+    //     start++;
+    //     end--;
+    // }
+    printarr(arr,n);
     
     return 0;
+    
 }

27eda127ebea464488d763a2a1cde6a7079ad1bb
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-03-21 17:04:51 +0530

    notes

--- assigment 4/productofarray_itself.cpp
+++ assigment 4/productofarray_itself.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+// int main() {
+//     int n;
+//     cin >> n;
 
-    int arr[n];
-    int output[n];
+//     int arr[n];
+//     int output[n];
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
     
-    output[0] = 1;
-    for(int i = 1; i < n; i++) {
-        output[i] = output[i-1] * arr[i-1];
-    }
+//     output[0] = 1;
+//     for(int i = 1; i < n; i++) {
+//         output[i] = output[i-1] * arr[i-1];
+//     }
 
 
-   int right = 1;
-    for(int i = n-1; i >= 0; i--) {
-        output[i] = output[i] * right;
-        right = right * arr[i];
-    }
-    for(int i = 0; i < n; i++) {
-        cout << output[i] << " ";
-    }
+//    int right = 1;
+//     for(int i = n-1; i >= 0; i--) {
+//         output[i] = output[i] * right;
+//         right = right * arr[i];
+//     }
+//     for(int i = 0; i < n; i++) {
+//         cout << output[i] << " ";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+

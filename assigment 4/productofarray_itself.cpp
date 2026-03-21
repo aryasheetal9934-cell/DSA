ed4c583d915da35767b0416282dcf603a49a8bb6
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-03-21 16:24:02 +0530

    notes

--- assigment 4/productofarray_itself.cpp
+++ assigment 4/productofarray_itself.cpp
@@ -24,7 +24,6 @@ int main() {
         output[i] = output[i] * right;
         right = right * arr[i];
     }
-
     for(int i = 0; i < n; i++) {
         cout << output[i] << " ";
     }
--- assigment 4/squareof_sortedarr.cpp
+++ assigment 4/squareof_sortedarr.cpp
@@ -1,20 +1,23 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-        arr[i] = arr[i] * arr[i];  
-    }
-    sort(arr, arr + n); 
+// int main() {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//         arr[i] = arr[i] * arr[i];  
+//     }
+//     sort(arr, arr + n); 
 
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    return 0;
-}
+//     for(int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+
 

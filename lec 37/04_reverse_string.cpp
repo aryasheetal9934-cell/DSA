8ea7b8d066277a5fffbb4c4d24e5bbc64c2e977d
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-03-24 22:58:10 +0530

    update

--- lec 37/04_reverse_string.cpp
+++ lec 37/04_reverse_string.cpp
@@ -1,17 +1,30 @@
+// #include <iostream>
+// #include<string>
+// using namespace std;
+
+// int main() {
+//     string s;
+//     cin>>s;
+//     int start=0,end=s.length()-1;
+//     while(start<end){
+//         swap(s[start],s[end]);
+//         start++;
+//         end--;
+//     }
+//     cout<<s;
+
+//     return 0;
+// }
 #include <iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
 int main() {
     string s;
     cin>>s;
-    int start=0,end=s.length()-1;
-    while(start<end){
-        swap(s[start],s[end]);
-        start++;
-        end--;
-    }
+    reverse(s.begin(),s.end());
     cout<<s;
-
+    
     return 0;
 }
